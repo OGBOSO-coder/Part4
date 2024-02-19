e0a4545213a4725c69cbf745bae139fadf2a6e1e
OGBOSO-coder <Kim.tiainen@gmail.com>
2024-02-19 10:29:54 +0200

    we

--- tests/dummy.test.js
+++
@@ -1,9 +0,0 @@
-const { test } = require('node:test')
-
-const { dummy } = require("../utils/list_helper");
-
-test("dummy return 1", () => {
-  const blogs = [];
-
-  expect(dummy(blogs)).toBe(1);
-});
