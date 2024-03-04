8d75ff140edf9996703cad608348374dc32c4917
OGBOSO-coder <Kim.tiainen@gmail.com>
2024-03-04 11:51:08 +0200

    kuinka monta ja ensimmöisen tunnistus

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -7,9 +8,23 @@ const api = supertest(app)
 
 test('notes are returned as json', async () => {
   await api
-    .get('api/blogs')
+    .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are 3 notes', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, 3)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/blogs')
+
+  const title = response.body.map(e => e.title)
+  // is the parameter truthy
+  assert(title.includes("Boso tuto"))
 })
 
 after(async () => {
