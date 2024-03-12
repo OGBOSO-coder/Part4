d74b2b8a1eb58e6006ce112c5cf301b2c6ddf5bd
OGBOSO-coder <Kim.tiainen@gmail.com>
2024-03-12 11:56:52 +0200

    sa

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,10 +1,34 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach} = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Blog = require("../models/blog");
 const api = supertest(app)
+
+
+const initialBlogs = [
+  {
+    title: "Excel Project",
+    author: "Excel",
+    url: "excel.url",
+    likes: 29,
+  },
+  {
+    title: "DevWithMe",
+    author: "DevMe",
+    url: "devme.url",
+    likes: 39,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let noteObject = new Blog(initialBlogs[0]);
+  await noteObject.save();
+  noteObject = new Blog(initialBlogs[1]);
+  await noteObject.save();
+});
 
 test('notes are returned as json', async () => {
   await api
@@ -16,7 +40,7 @@ test('notes are returned as json', async () => {
 test('there are 3 notes', async () => {
   const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, 3)
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 test('the first note is about HTTP methods', async () => {
@@ -24,8 +48,16 @@ test('the first note is about HTTP methods', async () => {
 
   const title = response.body.map(e => e.title)
   // is the parameter truthy
-  assert(title.includes("Boso tuto"))
+  assert(title.includes("Excel Project"))
 })
+
+test("blog post has 'id' property instead of '_id'", async () => {
+  const response = await api.get("/api/blogs");
+
+  const blog = response.body[0];
+  assert.strictEqual(typeof blog.id, 'string');
+});
+
 
 after(async () => {
   await mongoose.connection.close()
